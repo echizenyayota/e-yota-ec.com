8b196dabc75a9073b9e449b9ad34ac168e4856bd
echizenya <h.fqui35@gmail.com>
2019-03-14 17:16:26 +0900

    AMPページでプログレッシブウェブアプリ機能を有効にする（Service Worker をインストールしてオフライン アクセスを有効にする）（その4 sw.jsのistallイベントでアセットキャッシュするが失敗）

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/base.js',
+  '/base.css',
+  '/amp_favicon.png',
+  '/blocky-mountains-1.jpg',
+  '/blocky-mountains-1.jpg',
+  '/icons/home.png',
+  '/icons/Icon',
+  '/images/ecoteki_icon-128x128.png',
+  '/images/ecoteki_icon-144x144.png',
+  '/images/ecoteki_icon-152x152.png',
+  '/images/ecoteki_icon-192x192.png',
+  '/mountains_2x.jpg',
+  '/mountains-1.jpg',
+  '/mountains-2.jpg',
+  '/mountains-3.jpg',
+  '/mountains.jpg',
+];
 
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
