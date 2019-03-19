0612b31b1f5c95df034e5de62248b5d110fdd030
echizenya <h.fqui35@gmail.com>
2019-03-19 19:15:39 +0900

    AMPページでプログレッシブウェブアプリ機能を有効にする（Service Worker をインストールしてオフライン アクセスを有効にする）（その6 sw.jsのistallイベントでアセットキャッシュ成功）

--- sw.js
+++ sw.js
@@ -6,9 +6,8 @@ var urlsToCache = [
   '/base.css',
   '/amp_favicon.png',
   '/blocky-mountains-1.jpg',
-  '/blocky-mountains-1.jpg',
-  '/icons/home.png',
-  '/icons/Icon',
+  // '/icons/home.png',
+  // '/icons/Icon',
   '/images/ecoteki_icon-128x128.png',
   '/images/ecoteki_icon-144x144.png',
   '/images/ecoteki_icon-152x152.png',
@@ -26,7 +25,9 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        // return cache.addAll(urlsToCache);
+        // return urlsToCache.forEach(function (url) { cache.add(url).catch(function (e){ console.log("ERROR! : " + url); })});
+        return urlsToCache.forEach(function (url) { cache.add(url)});
       })
   );
 });
